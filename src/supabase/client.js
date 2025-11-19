24c5b9024f64470670d0245cb3b5af5f2bc0bede
techai342 <tiktokusr4400@gmail.com>
2025-11-19 19:44:14 +0500

    Update client.js

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://jwlpkgdgybihfubjztyw.supabase.com",
-  "YOUR_PUBLIC_ANON_KEY"
-);
+// YAHAN APNA SUPABASE URL AUR KEY DALIYE
+const supabaseUrl = "https://jwlpkgdgybihfubjztyw.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bHBrZ2RneWJpaGZ1Ymp6dHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ2ODU2MjcsImV4cCI6MjA1MDI2MTYyN30.8lTkQdKkq1v4KJNvVW9Q7VqY6Y6QwQ6Q9Q6Q9Q6Q9Q";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);

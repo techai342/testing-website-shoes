d4e883decaa24143f724ac96aac36d435a17f7c8
techai342 <tiktokusr4400@gmail.com>
2025-11-22 18:00:33 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-.import React from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import Navbar from './components/Navbar'
